d82d73f52ba975638d92b40bb18e8121f9fa6f1e
harleyszhang <zhg5200211@outlook.com>
2026-08-18 18:25:55 +0800

    feat: resolve qwen3 and qwen3-vl infer

--- lite_llama/kernels/utils.py
+++ lite_llama/kernels/utils.py
@@ -21,6 +21,8 @@ import triton.language as tl
 from packaging.version import Version
 
 
+MAX_FUSED_SIZE = 65536
+
 def is_hip() -> bool:
     return torch.version.hip is not None
 
@@ -47,9 +49,6 @@ def ensure_contiguous(fn):
 
 
 def calculate_settings(n):
-    # reference: https://github.com/unslothai/unsloth/blob/fd753fed99ed5f10ef8a9b7139588d9de9ddecfb/unsloth/kernels/utils.py#L43
-
-    MAX_FUSED_SIZE = 65536
     BLOCK_SIZE = triton.next_power_of_2(n)
     if BLOCK_SIZE > MAX_FUSED_SIZE:
         raise RuntimeError(
